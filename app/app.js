e00d5a0c2647efbad5b4563cddb7ece6d095019e
Victor Robert <rinmai2019@gmail.com>
2022-06-14 11:38:24 +0700

    up

---
+++ app/app.js
@@ -0,0 +1,15 @@
+window.onscroll = () => {
+  if (window.scrollY > 0) {
+    document.querySelector(".header").classList.add("active");
+  } else {
+    document.querySelector(".header").classList.remove("active");
+  }
+};
+
+window.onload = () => {
+  if (window.scrollY > 0) {
+    document.querySelector(".header").classList.add("active");
+  } else {
+    document.querySelector(".header").classList.remove("active");
+  }
+};
